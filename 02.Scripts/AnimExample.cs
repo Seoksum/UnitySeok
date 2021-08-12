2b2829ad6c41427eade65625cce9710bf68f0e8c
Seoksum <ssm9804@naver.com>
2021-08-12 17:37:26 +0900

    Project2019_Scripts

---
+++ 02.Scripts/AnimExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimExample : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField] private Transform lookObj;
+    [SerializeField] private Transform grabObj;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        anim.SetIKPosition(AvatarIKGoal.LeftHand, grabObj.position);
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+        anim.SetIKRotation(AvatarIKGoal.LeftHand, grabObj.rotation);
+
+        anim.SetLookAtWeight(1.0f);
+        anim.SetLookAtPosition(lookObj.position);
+    }
+
+}
