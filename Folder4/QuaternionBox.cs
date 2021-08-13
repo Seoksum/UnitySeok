d62e5119c3563f67e37b3fd62e4a5019a29b9312
Seoksum <ssm9804@naver.com>
2021-08-13 15:47:49 +0900

    ada

---
+++ Folder4/QuaternionBox.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaternionBox : QuaternionTest
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotate();
+    }
+    
+    
+
+}
